5abcf794d2c67a23b1e74c8fc44b92f6cad7e5ab
Simon Dean <simon@simondean.org>
2013-06-22 18:54:19 +0100

    Added Grunt.  Added the grunt-cuke-tree plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,21 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'features/**/*.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    clean: {
+      tests: ['tmp'],
+    },
+
     cuketree: {
       run: {},
       ide: {
@@ -10,10 +25,13 @@ module.exports = function(grunt) {
         },
       },
     },
+
   });
 
   grunt.loadNpmTasks('grunt-cuke-tree');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['cuketree:run']);
+  grunt.registerTask('default', ['jshint', 'cuketree:run']);
 
 };
