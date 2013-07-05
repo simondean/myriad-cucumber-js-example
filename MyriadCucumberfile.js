c59c81993b1b12d166106b7796c97948a61f61cd
Simon Dean <simon@simondean.org>
2013-07-06 00:01:37 +0100

    Works under Linux now

--- MyriadCucumberfile.js
+++ MyriadCucumberfile.js
@@ -2,14 +2,14 @@ module.exports = {
   package: '.',
   profiles: {
     busy_wait: {
-      bin: 'monkey_patches/.bin/cucumber-js',
+      bin: './monkey_patches/.bin/cucumber-js',
       args: ['-format', 'json', '-t', '~@wip', '--require', './features'],
       env: {
         WAIT: 'busy'
       }
     },
     sleep_wait: {
-      bin: 'monkey_patches/.bin/cucumber-js',
+      bin: './monkey_patches/.bin/cucumber-js',
       args: ['-format', 'json', '-t', '~@wip', '--require', './features'],
       env: {
         WAIT: 'sleep'
